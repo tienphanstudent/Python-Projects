fa246d5cb8252251467e06bed0ba347045e25f2b
tienphanstudent <47227293+tienphanstudent@users.noreply.github.com>
2020-06-16 23:59:09 -0400

    Update PythonSyntaxPractice.py

--- PythonSyntaxPractice.py
+++ PythonSyntaxPractice.py
@@ -1,6 +1,7 @@
 # Tien Phan
 # Practice of python syntax
 
+# defintions of furniture items with their prices
 lovely_loveseat_description = """Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or White."""
 lovely_loveseat_price = 254.00
 
@@ -12,6 +13,7 @@ luxurious_lamp_price = 52.15
 
 sales_tax = .088
 
+# creation of receipt for customer 1 and increment the customer's selection into the empty variables created 
 customer_one_total = 0
 customer_one_itemization = ""
 customer_one_total += lovely_loveseat_price
@@ -20,6 +22,7 @@ customer_one_total += luxurious_lamp_price
 customer_one_tax = customer_one_total * sales_tax
 customer_one_total += customer_one_tax
 
+# print to console the receipt of customer one
 print("Customer One Items:")
 print(customer_one_itemization)
 print(" ")
