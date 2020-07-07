9a96d337c88b5c1ec98f3bf252642367204ffbfb
tienphanstudent <tienphan@usf.edu>
2020-07-07 10:07:58 -0400

    Added test objects

---
+++ The Boredless Tourist.py
@@ -0,0 +1,7 @@
+# Tien Phan
+# The Boredless Tourist Project
+
+# definitions
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
+test_traveler = ["Erin Wilkes", "Shanghai, China", ["historical site", "art"]]
+
