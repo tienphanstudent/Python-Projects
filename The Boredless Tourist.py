f18774070533711c738e0d1f907f7464a25e4c63
tienphanstudent <tienphan@usf.edu>
2020-07-07 10:36:00 -0400

    Added logic to find traveler destinations and convert to internal data

--- The Boredless Tourist.py
+++ The Boredless Tourist.py
@@ -5,3 +5,26 @@
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
 test_traveler = ["Erin Wilkes", "Shanghai, China", ["historical site", "art"]]
 
+
+def get_destination_index(destination):
+    destination_index = ""
+    try:
+        destination_index = destinations.index(destination)
+    except ValueError:
+        print("The destination is not in the list")
+
+    return destination_index
+
+def get_traveler_location(traveler):
+    traveler_destination = test_traveler[1]
+    traveler_destination_index = ""
+
+    traveler_destination_index = get_destination_index(traveler_destination)
+
+    return traveler_destination_index
+
+
+test_destination_index = get_traveler_location(test_traveler)
+print(get_destination_index("Hyderabad, India"))
+print(test_destination_index)
+
